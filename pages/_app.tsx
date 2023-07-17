609e9585f993a95765282094722a0d93fbc4b33f
nguyenthanhan201 <fxannguyen201@gmail.com>
2023-07-17 20:27:34 +0700

    ci: add font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,4 @@
-// import { Roboto } from "@next/font/google";
+import { Roboto } from "@next/font/google";
 import { Fragment } from "react";
 
 type Props = {
@@ -6,20 +6,17 @@ type Props = {
   pageProps: any;
 };
 
-// const roboto = Roboto({
-//   weight: ["400", "500", "700"],
-//   subsets: ["vietnamese"],
-// });
+const roboto = Roboto({
+  weight: ["400", "500", "700"],
+  subsets: ["vietnamese"],
+});
 
 function MyApp({ Component, pageProps }: Props) {
-  console.clear();
-  console.log("👌  pageProps:", pageProps);
-  console.log("👌  Component:", Component);
   const Layout = Component.Layout ? Component.Layout : Fragment;
   const layoutProps = Component.LayoutProps ? Component.LayoutProps : {};
 
   return (
-    <main className={"roboto.className"}>
+    <main className={roboto.className}>
       <Layout {...layoutProps}>
         <Component {...pageProps} />
       </Layout>
